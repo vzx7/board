e4f2562cec8a873e5a3fda834b79d617a10f9c32
VZ Gentoo <coder.vz@gmail.com>
2017-06-26 09:52:07 +0700

    fixed

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,32 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
-    entry: './src/index.jsx',
-    output: { 
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public', 'js'),
-        publicPath: '/js/'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                loaders: ['react-hot', 'babel-loader']
-            },
-            {
-                test: /\.sass$/,
-                loaders: ['style-loader', 'css-loader', 'sass-loader']
-            }
-        ]
-    },
-    devtool: 'eval-source-map',
-    resolve: {
-        extensions: ['', '.js', '.jsx']
-    },
-    devServer: {
-        inline: true,
-        contentBase: path.resolve(__dirname, 'public'),
-        port: 8080
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public', 'js'),
+    publicPath: 'js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel-loader']
+      },
+      {
+        test: /\.sass$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  devtool: 'eval-source-map',
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  devServer: {
+    inline: true,
+    contentBase: path.resolve(__dirname, 'public'),
+    port: 8080
   }
-};
+}
